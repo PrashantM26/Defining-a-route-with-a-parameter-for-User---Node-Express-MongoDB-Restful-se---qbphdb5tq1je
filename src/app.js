7a6066064c2b0db097267ab81a1121fea78b523f
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-11-28 06:00:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,17 @@ const app = express();
 
 app.get('', (req, res) => {
   //Write a code here for endpoint /user/:userId and also print parameter in json form
+   const { userId } = req.params;
+
+  if (!userId) {
+    return res.status(400).json({
+      error: 'Invalid request. userId is missing.',
+    });
+  }
+
+  res.status(200).json({
+    userId,
+  });
 });
 
 module.exports = app;
