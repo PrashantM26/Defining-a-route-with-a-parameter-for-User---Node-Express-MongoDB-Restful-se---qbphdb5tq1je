1d9438c41b4e1702b51328ec7922d8eb35aab7a4
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-11-28 06:05:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('', (req, res) => {
-  //Write a code here for endpoint /user/:userId and also print parameter in json form
-   const { userId } = req.params;
+// GET route for retrieving user information by userId
+// Endpoint: /user/:userId
+app.get('/user/:userId', (req, res) => {
+  // Extract userId from route parameters
+  const { userId } = req.params;
 
+  // Check if userId is present
   if (!userId) {
     return res.status(400).json({
       error: 'Invalid request. userId is missing.',
     });
   }
 
+  // Return the userId in the response with the expected format
   res.status(200).json({
-    userId,
+    userId: userId,
   });
 });
 
